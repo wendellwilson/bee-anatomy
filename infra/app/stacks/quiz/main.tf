cd40a738b652ba9522c34628c0a3065e4f25b67a
Wendell Wilson <wendell.wilson@gmail.com>
2023-04-20 12:57:40 -0400

    fix s3 obj path

--- infra/app/stacks/quiz/main.tf
+++ infra/app/stacks/quiz/main.tf
@@ -1,5 +1,9 @@
+locals {
+  project_path = split("infra", abspath(path.root))[0]
+}
+
 module "bucket" {
   source     = "../../modules/s3site"
   bucket     = "${var.env}-${var.bucket_postfix}"
-  dist_directory = "../../../../../dist/"
+  dist_directory = "${local.project_path}dist/"
 }
